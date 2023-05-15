501f70ed0315a21889cd1884491e66ee9722ac80
fadingreality1 <kunalverma.learn@gmail.com>
2023-05-15 14:38:16 +0530

    forms have been designed

--- Task_Manager/views.py
+++ Task_Manager/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def home(req):
     return render(req, "home.html")
@@ -8,17 +8,20 @@ def signup(req):
     if req.method == "POST":
         f = UserCreationForm(req.POST)
         if f.is_valid():
-            # return HttpResponse("sahi h bhai")
             user = f.save()
             if user is not None:
-                redirect('home')
+                return redirect('login')
         else:
             return render(req, "signup.html", {'form':f})
     f = UserCreationForm()
     return render(req, "signup.html", {'form':f})
 
 def login(req):
-    return render(req, "login.html")
+    if req.method == "POST":
+        pass
+        
+    f = AuthenticationForm()
+    return render(req, "login.html", {'form':f})
 
 def contact(req):
     return render(req, "contact.html")
