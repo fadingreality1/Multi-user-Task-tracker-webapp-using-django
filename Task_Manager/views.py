6d675d06939263572776538d32f2bf32a8e85f92
fadingreality1 <kunalverma.learn@gmail.com>
2023-05-15 13:01:58 +0530

    users can be created

--- Task_Manager/views.py
+++ Task_Manager/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def home(req):
     return render(req, "home.html")
 
 def signup(req):
-    form = UserCreationForm()
-    return render(req, "signup.html", {'form':form})
+    if req.method == "POST":
+        f = UserCreationForm(req.POST)
+        if f.is_valid():
+            # return HttpResponse("sahi h bhai")
+            user = f.save()
+            if user is not None:
+                redirect('home')
+        else:
+            return render(req, "signup.html", {'form':f})
+    f = UserCreationForm()
+    return render(req, "signup.html", {'form':f})
 
 def login(req):
     return render(req, "login.html")
